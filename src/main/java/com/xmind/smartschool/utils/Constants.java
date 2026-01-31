6bec7c9e81029ef9b580a425203f59e59cf931ec
RyanNeuville <neuvilleryan123@gmail.com>
2026-01-31 13:55:50 +0100

    feat: Add database connection constants to a new `Constants` utility class.

---
+++ src/main/java/com/xmind/smartschool/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.xmind.smartschool.utils;
+
+public class Constants {
+    public static final String DB_URL = "jdbc:postgresql://localhost:5432/smartschool";
+    public static final String DB_USER = "codedrill";
+    public static final String DB_PASSWORD = "Codedrill@237";
+}
