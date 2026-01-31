08f8f58b66ffd9f5184af98d617ab2a582703cb7
RyanNeuville <neuvilleryan123@gmail.com>
2026-01-31 07:59:38 +0100

    feat: add Note model with fields, constructors, and accessors

---
+++ src/main/java/com/xmind/smartschool/models/Note.java
@@ -0,0 +1,83 @@
+package com.xmind.smartschool.models;
+
+import java.time.LocalDate;
+
+public class Note {
+    private Long id;
+    private Double valeur;
+    private String commentaire;
+    private LocalDate dateSaisie;
+    private Long etudiantId;
+    private Long evaluationId;
+
+    public Note() {
+    }
+
+    public Note(Long id, Double valeur, String commentaire, LocalDate dateSaisie, Long etudiantId, Long evaluationId) {
+        this.id = id;
+        this.valeur = valeur;
+        this.commentaire = commentaire;
+        this.dateSaisie = dateSaisie;
+        this.etudiantId = etudiantId;
+        this.evaluationId = evaluationId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getValeur() {
+        return valeur;
+    }
+
+    public void setValeur(Double valeur) {
+        this.valeur = valeur;
+    }
+
+    public String getCommentaire() {
+        return commentaire;
+    }
+
+    public void setCommentaire(String commentaire) {
+        this.commentaire = commentaire;
+    }
+
+    public LocalDate getDateSaisie() {
+        return dateSaisie;
+    }
+
+    public void setDateSaisie(LocalDate dateSaisie) {
+        this.dateSaisie = dateSaisie;
+    }
+
+    public Long getEtudiantId() {
+        return etudiantId;
+    }
+
+    public void setEtudiantId(Long etudiantId) {
+        this.etudiantId = etudiantId;
+    }
+
+    public Long getEvaluationId() {
+        return evaluationId;
+    }
+
+    public void setEvaluationId(Long evaluationId) {
+        this.evaluationId = evaluationId;
+    }
+
+    @Override
+    public String toString() {
+        return "Note [id=" + id
+                + ", valeur=" + valeur
+                + ", commentaire=" + commentaire
+                + ", dateSaisie=" + dateSaisie
+                + ", etudiantId=" + etudiantId
+                + ", evaluationId=" + evaluationId
+                + "]";
+    }
+}
