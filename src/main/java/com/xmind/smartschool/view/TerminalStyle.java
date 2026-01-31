433d2cfc69e61eba8df04ba1da80552969df3db4
RyanNeuville <neuvilleryan123@gmail.com>
2026-01-31 23:16:45 +0100

    feat: Add `TerminalStyle` class with constants for ANSI terminal colors and box-drawing characters.

---
+++ src/main/java/com/xmind/smartschool/view/TerminalStyle.java
@@ -0,0 +1,26 @@
+package com.xmind.smartschool.view;
+
+/*
+ * Terminal style class to provide terminal style constants.
+ */
+public class TerminalStyle {
+    /*
+     * Terminal colors textes.
+     */
+    public static final String RESET = "\u001B[0m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String RED = "\u001B[31m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String BOLD = "\u001B[1m";
+
+    /* Symboles and border */
+    public static final String TOP_LEFT = "┌";
+    public static final String TOP_RIGHT = "┐";
+    public static final String BOTTOM_LEFT = "└";
+    public static final String BOTTOM_RIGHT = "┘";
+    public static final String HORIZONTAL = "─";
+    public static final String VERTICAL = "│";
+}
