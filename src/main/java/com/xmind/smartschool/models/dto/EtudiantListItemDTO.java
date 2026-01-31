800d5327f5c2b2242c49a85ca694604ed96e0d62
RyanNeuville <neuvilleryan123@gmail.com>
2026-01-31 12:31:44 +0100

    feat: Add EtudiantListItemDTO for representing student list items with matricule, name, average, and status.

---
+++ src/main/java/com/xmind/smartschool/models/dto/EtudiantListItemDTO.java
@@ -0,0 +1,66 @@
+package com.xmind.smartschool.models.dto;
+
+public class EtudiantListItemDTO {
+    public enum Statut {
+        ACTIF,
+        INACTIF,
+        SUSPENDU
+    }
+
+    private String matricule;
+    private String nomPrenom;
+    private String moyenneAnnuelle;
+    private Statut statut;
+
+    public EtudiantListItemDTO() {
+    }
+
+    public EtudiantListItemDTO(String matricule, String nomPrenom, String moyenneAnnuelle, Statut statut) {
+        this.matricule = matricule;
+        this.nomPrenom = nomPrenom;
+        this.moyenneAnnuelle = moyenneAnnuelle;
+        this.statut = statut;
+    }
+
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
+
+    public String getNomPrenom() {
+        return nomPrenom;
+    }
+
+    public void setNomPrenom(String nomPrenom) {
+        this.nomPrenom = nomPrenom;
+    }
+
+    public String getMoyenneAnnuelle() {
+        return moyenneAnnuelle;
+    }
+
+    public void setMoyenneAnnuelle(String moyenneAnnuelle) {
+        this.moyenneAnnuelle = moyenneAnnuelle;
+    }
+
+    public Statut getStatut() {
+        return statut;
+    }
+
+    public void setStatut(Statut statut) {
+        this.statut = statut;
+    }
+
+    @Override
+    public String toString() {
+        return "EtudiantListItemDTO [matricule=" + matricule
+                + ", nomPrenom=" + nomPrenom
+                + ", moyenneAnnuelle=" + moyenneAnnuelle
+                + ", statut=" + statut
+                + "]";
+    }
+
+}
